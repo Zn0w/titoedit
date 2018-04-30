6a5458509124519213148aac7c5a95f6ebd19d08
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-04-30 10:37:26 +0300

    started to work on raw mode user input

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
-	printf("Hello World!\n");
-
-	getchar();
+	char c;
+	
+	//listen to the user input and then print it out
+	while(read(STDIN_FILENO, &c, 1) == 1)
+	{
+		printf("%c", c);
+	}
 	
 	return 0;
 }
