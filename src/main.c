84893cf40de870723cb406da81b6f6b245ad59b3
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-04-30 10:49:43 +0300

    working on enabling raw mode in the terminal

--- src/main.c
+++ src/main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+
+void enableTerminalRawMode()
+{
+	struct termios raw_mode;
+
+	// Get current terminal's attributes
+	tcgetattr(STDIN_FILENO, &raw_mode);
+
+	// Turn off echoing
+	raw_mode.c_lflag &= ~(ECHO);
+
+	// Set new attributes to the terminal
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_mode);
+}
 
 int main()
 {
+	enableTerminalRawMode();
+	
 	char c;
 	
 	//listen to the user input and then print it out
-	while(read(STDIN_FILENO, &c, 1) == 1)
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
 	{
 		printf("%c", c);
 	}
